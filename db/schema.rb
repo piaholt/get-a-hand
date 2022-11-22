64c402783972ed3b6f9dc32093c8aad1c8b26dcf
Mateo Westphal <mateojwestphal@gmail.com>
2022-11-22 13:00:00 +0000

    form and new for User view

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_171018) do
-
-
+ActiveRecord::Schema[7.0].define(version: 2022_11_22_105220) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,10 +49,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_171018) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_services_on_user_id"
-
   end
 
   create_table "users", force: :cascade do |t|
