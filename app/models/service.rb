a5144aa774e6ebcdb771ad343f2f9b42171b39d7
Mateo Westphal <mateojwestphal@gmail.com>
2022-11-22 14:57:45 +0000

    changed service model from has_one relation to Belongs_to user

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,7 +1,7 @@
 class Service < ApplicationRecord
   has_one_attached :photo
   has_many :bookings
-  has_one :user
+  belongs_to :user
   validates_associated :user
   validates :category, :description, :title, :price, presence: true
 end
