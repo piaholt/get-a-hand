2134b023ad8dfc2b0c22bfc3188ed9aa93711405
(Ben Kaefer)benkaefer@icloud.com <benkaefer@icloud.com>
2022-11-22 12:56:29 +0000

    booking migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_171018) do
-
-
+ActiveRecord::Schema[7.0].define(version: 2022_11_22_125151) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,6 +42,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_171018) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "service_id"
+    t.date "end_date"
+    t.date "starting_date"
+    t.boolean "confirmed", default: false
+    t.boolean "pending", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["service_id"], name: "index_bookings_on_service_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
   create_table "services", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -52,10 +62,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_171018) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_services_on_user_id"
-
   end
 
   create_table "users", force: :cascade do |t|
@@ -77,5 +85,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_171018) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookings", "services"
+  add_foreign_key "bookings", "users"
   add_foreign_key "services", "users"
 end
