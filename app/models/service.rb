aa98e48bcf482852f314243a0bd4b0d6426c8d90
Charleen Ipping Wah <charleen.ipping@gmail.com>
2022-11-22 12:59:38 +0000

    added validation to service model

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,8 @@
 class Service < ApplicationRecord
   has_one_attached :photo
+
+  has_many :bookings
+  has_one :user
+  validates_associated :user
+  validates :category, :description, :title, :price, presence: true
 end
