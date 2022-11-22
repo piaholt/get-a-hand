0d4963aa83addc32e22bde1dff4a8d48384e5ff2
Mateo Westphal <mateojwestphal@gmail.com>
2022-11-22 14:47:02 +0000

    Added validations to service model

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,7 @@
 class Service < ApplicationRecord
   has_one_attached :photo
+  has_many :bookings
+  has_one :user
+  validates_associated :user
+  validates :category, :description, :title, :price, presence: true
 end
