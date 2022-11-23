6c304720d9c1ae78687af3880a414a5315597412
(Ben Kaefer)benkaefer@icloud.com <benkaefer@icloud.com>
2022-11-23 11:54:57 +0000

    commiting

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :bookings, only: %i[new create]
   end
   # resources :bookings
-  get "/bookings", to: "bookings#my_bookings", as: :my_bookings
+  get "/my_bookings", to: "bookings#my_bookings", as: :my_bookings
   # get "/user", to: "users#show", as: :user
   # get "/user/edit", to: "users#edit", as: :edit_user
   # patch  "/user", to: "users#update"
