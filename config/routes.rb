842fa79ab53484cfa453a63204a28db67e80949d
Charleen Ipping Wah <charleen.ipping@gmail.com>
2022-11-23 12:09:06 +0000

    User is now able to edit its profile and is saved

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  resources :users, only: %i[show]
   resources :services do
     resources :bookings, only: %i[new create]
   end
