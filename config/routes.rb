d35c18a40fa77b089c880a3870e3899c882408f6
Charleen Ipping Wah <charleen.ipping@gmail.com>
2022-11-23 11:37:26 +0000

    Removed user route as was conflicting with Devise

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,10 @@ Rails.application.routes.draw do
   resources :services do
     resources :bookings, only: %i[new create]
   end
-  resources :bookings
+  # resources :bookings
   get "/bookings", to: "bookings#my_bookings", as: :my_bookings
-  get "/user", to: "users#show", as: :user
-  get "/user/edit", to: "users#edit", as: :edit_user
-  patch  "/user", to: "users#update"
+  # get "/user", to: "users#show", as: :user
+  # get "/user/edit", to: "users#edit", as: :edit_user
+  # patch  "/user", to: "users#update"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  get "my_bookings", to: "bookings#my_bookings", as: :my_bookings
 end
