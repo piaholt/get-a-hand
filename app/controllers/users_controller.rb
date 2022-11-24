95cb2392224470a60105a56d37e54cf58bbb2305
Pia Holtappels <pia.holtappels@icloud.com>
2022-11-24 14:43:02 +0000

    save changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,19 +7,19 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    @user = current_user
   end
 
   def update
-    @user = User.find(params[:id])
+    @user = current_user
     @user.update(user_params)
-    redirect_to user_path(@user)
+    redirect_to profile_path(@user)
   end
 
 
   private
 
   def user_params
-    params.require(:user).permit(:username, :first_name, :last_name, :location, :email)
+    params.require(:user).permit(:username, :first_name, :last_name, :location, :email, :photo)
   end
 end
